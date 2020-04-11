67c8c8a57895f79d7fe0fcc555a9ce7d7a81d7d1
ariela tolosa <ariela.tolosa.silva@gmail.com>
2020-04-10 21:32:24 -0400

    nuevos cambios

--- src/cipher.js
+++ src/cipher.js
@@ -9,6 +9,9 @@ window.cipher = {
       let posicion;
 
       if (ascii >= 65 && ascii <= 90) {
+        if(ascii == 90){ //si es Z
+          i=0;
+        }
         posicion = (ascii - 65 + parseInt(offset)) % 26 + 65;
   
       }else{
@@ -29,7 +32,7 @@ window.cipher = {
       let posicion;
   //con mayusculas
       if (asciI >= 65 && asciI <= 90) {
-        //con espacio
+      
         posicion = (asciI - 65 - parseInt(offset)) % 26 + 65;
         
       }else{ 
